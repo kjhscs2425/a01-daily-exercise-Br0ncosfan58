227c3e10a195007c685a004a5d89338564909daf
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2024-09-16 00:00:57 -0700

    a01 exercise

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 import turtle
 
+def draw_hexagon(t, side_length):
+    for i in range(6):
+        t.forward(side_length)
+        t.left(60)
+
+
+def draw_triangle(t, side_length):
+    for i in range(3):
+        turtle.forward(side_length)
+        turtle.left(120)
+
+def draw_hexagon_with_triangles(t, side_length):
+    draw_hexagon(t, side_length)
+
+    for i in range(5):
+        turtle.forward(side_length)
+        turtle.left(120)
+        draw_triangle(t, side_length)
+        turtle.right(60)
+
+def main():
+    window = turtle.Screen()
+
+    t = turtle.Turtle()
+    turtle.speed(5)
+    side_length = 100
+
+   
+  
+    draw_hexagon_with_triangles(t, side_length)
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
